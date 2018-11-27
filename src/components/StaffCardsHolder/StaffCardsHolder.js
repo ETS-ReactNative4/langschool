3e823d2864c734002a881fe314489615396649e3
Daily120 <forzhacker@mail.ru>
2018-11-27 21:56:56 +0100

    staffCardsHolder mockup

---
+++ src/components/StaffCardsHolder/StaffCardsHolder.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './StaffCardsHolder.css';
+
+const StaffCardsHolder = () => {
+
+    return (
+        <div id="staffCardsHolder">
+            <div className="staffCardsCollection">
+
+            </div>
+        </div>
+    )
+}
+
+export default StaffCardsHolder;
