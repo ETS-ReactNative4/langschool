29af9277a08fa17165ea76f77e50f8afc40cd919
Yerassyl <erasyl.boy@gmail.com>
2018-11-28 00:54:50 +0600

    header done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,19 @@ const Header = () => {
 
     return (
         <div id="header">
-
+            <div className="firstComponent">
+                <p>First</p>
+            </div>
+            <div className="secondComponent">
+                <a href="">Главная</a>
+                <a href="">Стафф</a>
+                <a href="">Галерея</a>
+                <a href="">Контакты</a>
+            </div>
+            <div className="thirdComponent">
+                <p>848848484</p>
+                <button className="applyButton">Оформить заявку</button>
+            </div>
         </div>
     )
 }
