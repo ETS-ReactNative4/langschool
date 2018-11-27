0a6a42ea0aa07bbeca9acd4da3ebb25da3b45f90
Daily120 <forzhacker@mail.ru>
2018-11-27 18:52:20 +0100

    header into banner

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Header from './components/Header/Header';
+import Banner from './components/Banner/Banner';
 
 class App extends Component {
   constructor() {
@@ -13,7 +13,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
+        <Banner />
       </div>
     );
   }
---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Banner.css';
+import Header from '../Header/Header';
+
+const Banner = () => {
+
+    return (
+        <div id="banner">
+            <Header />
+            <div className="bannerContent">
+
+            </div>
+            <div className="arrowDown">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Banner;
