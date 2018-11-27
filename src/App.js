0b17877d4d98c2d862e44c776754d1366d772916
Daily120 <forzhacker@mail.ru>
2018-11-27 18:37:51 +0100

    added playground for first block(banner)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './components/Header/Header';
 
 class App extends Component {
   constructor() {
@@ -12,7 +13,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Header />
       </div>
     );
   }
