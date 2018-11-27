f1099fed5e1a1c7d344d5ab994227b4ff2e7f81b
Daily120 <forzhacker@mail.ru>
2018-11-27 21:21:15 +0100

    finished banner component

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -8,7 +8,14 @@ const Banner = () => {
         <div id="banner">
             <Header />
             <div className="bannerContent">
-
+                <div id="bannerInfo">
+                    <p>Английский Лагерь в Подмосковье</p>
+                    <p>Тренинги по английскому языку, творческие проекты, мастер-классы и многое другое!</p>
+                    <div className="bannerBtnWrapper">
+                        <a className="btn yellow-btn" href="#">Ближайшие смены</a>
+                        <a className="btn transparent-btn" href="#">Оформить заявку</a>
+                    </div>
+                </div>
             </div>
             <div className="arrow">
                 <div className="arrowDown"></div>
