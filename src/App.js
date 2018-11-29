268a3c9199badafb8262c17620977ab91b4a5bbb
Yerassyl <erasyl.boy@gmail.com>
2018-11-29 06:40:24 +0600

    fixed conflicts

--- src/App.js
+++ src/App.js
@@ -6,12 +6,9 @@ import StaffCardsHolder from './components/StaffCardsHolder/StaffCardsHolder';
 import Portfolio from './components/Portfolio/Portfolio';
 import Footer from './components/Footer/Footer';
 import SeasonCardHolder from './components/SeasonCardHolder/SeasonCardHolder';
-<<<<<<< HEAD
 import Form from './components/Form/Form';
-=======
 import SideBarCaller from './components/SideBarCaller/SideBarCaller';
 import SideBar from './components/SideBar/SideBar';
->>>>>>> 49002edc675ae99126a24b587e29cdefe50d6495
 
 class App extends Component {
   constructor() {
