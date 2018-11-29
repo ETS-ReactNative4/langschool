d568921e5a22ca88d0621e0fd00bc6a6f6667f63
Yerassyl <erasyl.boy@gmail.com>
2018-11-29 07:38:06 +0600

    Updates

---
+++ precache-manifest.ed16c5b36727cf2b5dbfc4d7eaea35a5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "01673d767bd2d1a50c0b25ed6bbfa7cb",
+    "url": "/langschool/static/media/woman.01673d76.jpg"
+  },
+  {
+    "revision": "2dbb4029c6ff09ccc79683011bfbf303",
+    "url": "/langschool/static/media/winter2.2dbb4029.jpg"
+  },
+  {
+    "revision": "72fd5e8963fc51cf3af68eb329f3b3bc",
+    "url": "/langschool/static/media/winter.72fd5e89.jpg"
+  },
+  {
+    "revision": "28416b61445636c87188a1bee4aca8bf",
+    "url": "/langschool/static/media/photo.28416b61.jpg"
+  },
+  {
+    "revision": "4c486fed940cf46b5c2d29b9563beda0",
+    "url": "/langschool/static/media/logo.4c486fed.jpg"
+  },
+  {
+    "revision": "5a508ba0d0b407dafbb2",
+    "url": "/langschool/static/js/runtime~main.5a508ba0.js"
+  },
+  {
+    "revision": "3c376fa2bdd9abf40cef",
+    "url": "/langschool/static/js/main.3c376fa2.chunk.js"
+  },
+  {
+    "revision": "92df97a767ad4e3a68f7",
+    "url": "/langschool/static/js/1.92df97a7.chunk.js"
+  },
+  {
+    "revision": "3c376fa2bdd9abf40cef",
+    "url": "/langschool/static/css/main.cc09b3f0.chunk.css"
+  },
+  {
+    "revision": "b43ad8eb07ba18dffc277c1b58cd8ba3",
+    "url": "/langschool/index.html"
+  }
+];
