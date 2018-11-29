034b8fcb794e3c228c53860853a8c240e5c4f8c9
Yerassyl <erasyl.boy@gmail.com>
2018-11-30 00:19:00 +0600

    footer improvements

--- src/components/SeasonsCard/SeasonsCard.js
+++ src/components/SeasonsCard/SeasonsCard.js
@@ -12,7 +12,7 @@ const SeasonsCard = () => {
                 <p>Зимний английский лагерь в Подмосковье для детей от 7 до 17 лет</p>
             </div>
             <div className="heiwid">
-                <button className="aboutButton">Оформить заявку</button>
+                <button className="applicationButton">Оформить заявку</button>
             </div>
         </div>
     )
