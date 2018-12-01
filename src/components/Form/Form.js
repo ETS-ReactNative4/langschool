d78843bf0cb6c5e946c9a645044a6b5b0f9221b6
Daily120 <forzhacker@mail.ru>
2018-12-01 20:32:54 +0100

    fixed form component in mobile devices

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -14,7 +14,7 @@ const Form = () => {
                 <p>Ваш телефон</p> 
                 <input className="twoForm" type="tel" placeholder="+7 (777) 792-848-24" name="PhoneNum" required/>
                 <p>Комментарий</p>
-                <textarea className="comments" name="comments" id="form1" cols="50" rows="4" placeholder="Оставьте комментарий..."></textarea>
+                <textarea className="comments" name="comments" id="textarea" cols="50" rows="4" placeholder="Оставьте комментарий..."></textarea>
                 <input className="submitButton" type="submit" value="Отправить" />
             </form>
             <p>Нажимая кнопку "Отправить" Вы даете согласие на обработку своих персональных данных</p>
