92e84841d7cc5e74a8b4192c72565800ab92eee8
Daily120 <forzhacker@mail.ru>
2018-12-04 17:20:43 +0100

    added another seasoncard

--- src/components/SeasonCardHolder/SeasonCardHolder.js
+++ src/components/SeasonCardHolder/SeasonCardHolder.js
@@ -7,9 +7,9 @@ const SeasonCardHolder = () => {
     return (
         <div id="seasonCardHolder"> 
             <div className="seasonCardCollection">
-                <SeasonsCard />
-                <SeasonsCard />
-                <SeasonsCard />
+                <SeasonsCard image='https://static.tildacdn.com/tild3634-6135-4965-a162-656634323636/WINTER_INSTA.jpg' name='Зимняя смена 15-18 декабря 2018' description='Английский лагерь в Боровом для детей от 7 до 16 лет'/>
+                <SeasonsCard image='https://static.tildacdn.com/tild3634-6135-4965-a162-656634323636/WINTER_INSTA.jpg' name='Зимняя смена 2-8 января 2019' description='Английский лагерь в Боровом для детей от 7 до 16 лет'/>
+                <SeasonsCard image='https://static.tildacdn.com/tild3634-6135-4965-a162-656634323636/WINTER_INSTA.jpg' name='Зимняя смена 3-7 января 2019' description='Английский лагерь в Астане для детей от 5 до 12 лет'/>
             </div>
         </div>
     )
--- src/components/SeasonsCard/SeasonsCard.js
+++ src/components/SeasonsCard/SeasonsCard.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import './SeasonsCard.css';
-import image from './img/winter.jpg';
 
-const SeasonsCard = () => {
+const SeasonsCard = ({ image, name, description }) => {
 
     return (
         <div className="seasonCard">
             <img src={image} alt="Winter Season"/>
             <div className="cardText">
-                <p>Зимняя смена 2-8 января 2019</p>
-                <p>Зимний английский лагерь в Подмосковье для детей от 7 до 17 лет</p>
+                <p>{name}</p>
+                <p>{description}</p>
             </div>
             <div className="heiwid">
                 <button className="applicationButton"><a href="#form">Оформить заявку</a></button>
