184150c5c340be63ad6f933daab62c0b46ff1d59
Yerassyl <erasyl.boy@gmail.com>
2019-03-14 20:30:27 +0600

    .

--- src/components/SeasonCardHolder/SeasonCardHolder.js
+++ src/components/SeasonCardHolder/SeasonCardHolder.js
@@ -10,7 +10,7 @@ const SeasonCardHolder = () => {
     return (
         <div id="seasonCardHolder"> 
             <div className="seasonCardCollection">
-                <SeasonsCard image={image15} name='Весенняя смена 21-30 марта' description='Английский лагерь в Боровом для детей от 7 до 16 лет'/>
+                <SeasonsCard image={image15} name='Весенняя смена 21-30 марта' description='Английский лагерь в Боровом для детей от 10 до 16 лет'/>
                 <SeasonsCard image={image3} name='Весенняя смена 25-29 марта' description='Английский лагерь в Астане для детей от 5 до 12 лет'/>
                 <SeasonsCard image={image2} name='Зимняя смена 16-17 февраля' description='Английский лагерь в Боровом для детей от 7 до 16 лет'/>
             </div>
